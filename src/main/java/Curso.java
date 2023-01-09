2941e017b8dbb0fb38793478907ad5e3a2e1b858
Juan Fernandes <juanfernandesrrm@gmail.com>
2023-01-09 11:34:32 -0300

    add examples of Optional

--- src/main/java/Curso.java
+++ src/main/java/Curso.java
@@ -1,6 +1,6 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Curso {
     private String nome;
@@ -43,5 +43,22 @@ class ExemploCursos{
                 .mapToInt(Curso::getAlunos)//retorna um IntStream
                 .sum();
 
+        cursos.stream()
+                .filter(c -> c.getAlunos() >= 100)
+                .findAny(). //retorna um Optional
+                ifPresent(curso -> System.out.println(curso.getNome()));
+
+        cursos = cursos.stream()
+                .filter(c -> c.getAlunos() >= 100)
+                .collect(Collectors.toList()); //Retorna uma lista
+
+        Map<String, Integer> collect = cursos.stream()
+                .filter(c -> c.getAlunos() >= 100)
+                .collect(Collectors.toMap(Curso::getNome, Curso::getAlunos));
+
+        cursos.stream()
+                .filter(c -> c.getAlunos() >= 100)
+                .collect(Collectors.toMap(Curso::getNome, Curso::getAlunos))
+                .forEach((nome, alunos) -> System.out.println("Curso "+nome+" tem "+alunos+" alunos"));//biconsumer
     }
 }
